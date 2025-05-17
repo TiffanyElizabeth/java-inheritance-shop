5ba821746b6bd08eddd04e48509421e4df04ed2d
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-05-17 14:55:32 +0200

    changed IMEI to user-given not randomly generated

--- src/products/Smartphone.java
+++ src/products/Smartphone.java
@@ -7,21 +7,24 @@ public class Smartphone extends Prodotto {
     private int memory;
 
     // constructor
-    public Smartphone(String name, String brand, double basePrice, double vat, int memory) {
+    public Smartphone(String name, String brand, double basePrice, double vat, String imei, int memory) {
         super(name, brand, basePrice, vat);
-        this.imei = getRandomNumber(15);
+        // this.imei = getRandomNumber(15);
+        this.imei = imei;
         this.memory = memory;
     }
 
-    // method to generate random 15-digit number for IMEI code
-    private static Random rnd = new Random();
+    // method to generate random 15-digit number for IMEI code COMMENTED BECAUSE
+    // REALIZED WE DO NOT NEED TO GENERATE IMEI
+    // private static Random rnd = new Random();
 
-    public static String getRandomNumber(int digCount) {
-        StringBuilder sb = new StringBuilder(digCount);
-        for (int i = 0; i < digCount; i++)
-            sb.append((char) ('0' + rnd.nextInt(10)));
-        return sb.toString();
-    } // https://stackoverflow.com/questions/3709521/how-do-i-generate-a-random-n-digit-integer-in-java-using-the-biginteger-class
+    // public static String getRandomNumber(int digCount) {
+    // StringBuilder sb = new StringBuilder(digCount);
+    // for (int i = 0; i < digCount; i++)
+    // sb.append((char) ('0' + rnd.nextInt(10)));
+    // return sb.toString();
+    // } //
+    // https://stackoverflow.com/questions/3709521/how-do-i-generate-a-random-n-digit-integer-in-java-using-the-biginteger-class
 
     // getters and setters
     public String getImei() {
